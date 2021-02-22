6acb321ebe86c45a5dd9f7229ef82f5e80f80958
Daniel <danieldornbusch@gmx.net>
2021-02-22 18:21:34 +0100

    Adding functionT

--- hotznplots.py
+++ hotznplots.py
@@ -134,24 +134,83 @@ def function(rows, *df, x_min=None, x_max=None, swap=False,
     # iterate through df in args and plot the rows
     
     for i in df:
-        ax = fig.add_subplot(1, 2, c)  # create subplot
+        ax = fig.add_subplot(1, len(df), c)  # create subplot
         if swap:
             i = i.T
+            x_min, x_max, y_min, y_max = y_min, y_max, x_min, x_max
         for r in rows:
             x = list(i.loc[:, x_min:x_max].columns)  # get x-values
             y = pd.DataFrame.to_numpy(i.loc[r, x_min:x_max])  # get y-values
-            ax.plot(x, y, linestyle='--', marker='x', color=colors[k],
+            ax.plot(x, y, linestyle='-', marker=' ', color=colors[k],
                     linewidth=1, label=i
                     )
             k = k + 1
             ax.legend(rows)
-
+        k = 0
+        # plots line into graph
+        ax.plot([20, 90], [0, 0], color='k', linestyle=':', linewidth=1)
         ax.set_xlabel(x_label)  # Add an x-label to the axes.
         ax.set_ylabel(y_label)  # Add a y-label to the axes.
         title = "fig." + str(c)
         c = c + 1
         plt.title(title)
-
+        
+def functionT(rows, *df, x_min = None, x_max = None, y_min = None, y_max = None, swap = True,
+             x_label="Wavelength[nm]", y_label="CD values[mdeg]", y_scaling = None, baseline = None, line1 = None, line2 = None, line3 = None, line4 = None, line5 = None
+             ):
+    
+    # create color list and color variables
+    colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+    k, c  = 0, 1
+    plt.ion()
+    fig = plt.figure()  # create figure
+    # iterate through df in args and plot the rows
+    
+    for i in df:
+        ax = fig.add_subplot(1, len(df), c)  # create subplot
+        if swap:
+            i = i.T
+        # get min, max values if not given
+        if x_min is None:
+            x_min = i.columns[0]
+        if x_max is None:
+            x_max = i.columns[-1]
+        if y_min is None:
+            y_min = i.min()
+        if y_max is None:
+            y_max = i.max()
+        
+        for r in rows:
+            x = list(i.loc[:, x_min:x_max].columns)  # get x-values
+            y = pd.DataFrame.to_numpy(i.loc[r, x_min:x_max])  # get y-values
+            ax.plot(x, y, linestyle='-', marker=' ', color=colors[k],
+                    linewidth=1, label=i
+                    )
+            k = k + 1
+            ax.legend(rows)
+        k = 0
+        
+        if y_scaling is not None:         
+            ax.set_ylim(y_scaling) # Y-achis scaling
+        #ax.set_ylim([y_min,y_max]) # Y-achis auto-scaling
+        if baseline is True: 
+            ax.plot([210, x_max], [0, 0], color='k', linestyle='-', linewidth=1) # plots baseline into graph
+        if line1 is not None:
+            ax.plot([210, x_max], line1, color='k', linestyle=':', linewidth=1) # plots line into graph
+        if line2 is not None:
+            ax.plot([210, x_max], line2, color='k', linestyle=':', linewidth=1)
+        if line3 is not None:
+            ax.plot([210, x_max], line3, color='k', linestyle=':', linewidth=1)
+        if line4 is not None:
+            ax.plot([210, x_max], line4, color='k', linestyle=':', linewidth=1)
+        if line5 is not None:
+            ax.plot([210, x_max], line5, color='k', linestyle=':', linewidth=1)
+        ax.set_xlabel(x_label)  # Add an x-label to the axes.
+        ax.set_ylabel(y_label)  # Add a y-label to the axes.
+        title = "Fig. " + str(c)
+        c = c + 1
+        plt.title(title)   
+    
     widgets.AppLayout(
         center=fig.canvas,
         footer=widgets.Button(icon='check'),
